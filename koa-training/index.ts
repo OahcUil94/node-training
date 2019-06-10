7505db3c39f65cafa954df70592513681e475e5b
OahcUil94 <oahc.uil.mail@gmail.com>
2019-06-10 18:11:13 +0800

    [add] add koa to index.ts

--- koa-training/index.ts
+++ koa-training/index.ts
@@ -1,1 +1,10 @@
-console.log('sfdsf');
+import * as Koa from 'koa';
+
+const app = new Koa();
+const port = process.env.PORT || 3000;
+
+app.use(async (ctx) => {
+  ctx.body = 'hello world';
+});
+
+app.listen(port);
